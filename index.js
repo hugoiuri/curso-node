8a1347b2ee020f338cfc3280b8129e7e61338e7e
hugoiuri <hugoraiuri@gmail.com>
2018-12-03 20:26:03 -0200

    criação do projeto

---
+++ index.js
@@ -0,0 +1,24 @@
+const pkg = require('./package.json');
+
+process.title = pkg.name;
+
+const shutdown = async () => {
+  console.info('Gracefully shutdown in progress');
+  await server.stop();
+  process.exit(0);
+};
+
+process.on('SIGTERM', shutdown)
+  .on('SIGINT', shutdown)
+  .on('SIGHUP', shutdown)
+  .on('uncaughtException', err => {
+    console.error('uncaughtException caught the error: ', err);
+    throw err;
+  })
+  .on('unhandledRejection', (err, promise) => {
+    console.error(`Unhandled Rejection at: Promise ${promise} reason: ${err}`);
+    throw err;
+  })
+  .on('exit', (code) => {
+    console.info(`Node process exit with code: ${code}`);
+  });
